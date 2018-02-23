8d277789f9464b96c4721179484b16a18ccaa8ca
Zausa Giulio <gzausa@lab050101.dsi.unive.it>
2018-02-23 11:36:59 +0100

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+// Loads express and storage library
+const express = require('express');
+const storage = require('node-persist');
+
+// Holds the current views
+var counter = 0;
+
+// Create a new Express app instance
+var app = express();
+
+// Registers an event triggered on HTTP GET /visit
+app.get("/visit", (req, res) => {
+    counter++;
+    
+    // Saves counter into the store and send response AFTER the store has been saved
+    storage.setItem("counter", counter).then(() => {
+        res.json(counter);
+    });
+});
+
+// Inits permanent storage and reads the saved counter
+storage.init().then(() => storage.getItem("counter")).then((value) => {
+    // Checks if value read is valid, otherwise set it to 0
+    if (value > 0) {
+        counter = value;
+    } else {
+        counter = 0;
+    }
+
+    // Start the web server, listening on port 8080, AFTER the counter has been read
+    app.listen(8080, "0.0.0.0");
+});
